2ca5da146286c1a25b276508b9e2b6bc24920efd
mbimoad <bmofunc>
2026-06-20 15:26:35 +0700

    Update

---
+++ .history/custom_20260620152623.js
@@ -0,0 +1,18 @@
+// Get the current URL's search parameters
+const params = new URLSearchParams(window.location.search);
+
+// Get the specific 'inv' value
+const invValue = params.get('inv'); 
+const malePria = params.get('fam'); 
+
+if(malePria.toLowerCase() == "true") {
+    document.querySelectorAll('#male_pria').forEach(item => item.innerHTML = 'Adam')
+}
+
+document.getElementById('nmtamu').innerHTML = invValue; 
+
+
+setTimeout(() => {
+    document.querySelector('.bmoloader').classList.add('hiding')
+}, 1000);
+
